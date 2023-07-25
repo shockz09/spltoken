cba172fe978afa53819594d1f1512dd0eaecb4a6
Rohit <rohit200827@gmail.com>
2023-07-25 19:46:39 +0530

    hehe

--- pages/heh.tsx
+++ pages/heh.tsx
@@ -56,7 +56,7 @@ const Heh: FC = ({ }) => {
   const [isChecked, setIsChecked] = useState(false);
   const [metadataMethod, setMetadataMethod] = useState('url')
   const [tokenDescription, setTokenDescription] = useState('')
-  const [img,setImg] = useState(false)
+  const [img, setImg] = useState(false)
   const [file, setFile] = useState<Readonly<{
     buffer: Buffer;
     fileName: string;
@@ -112,95 +112,70 @@ const Heh: FC = ({ }) => {
               <div className="md:w-[600px] mx-auto">
                 <div className="md:w-[480px] flex flex-col m-auto">
 
-
-                  <label className="flex text-white ">Token Name</label>
-
-                  <Input type="text" onChange={(e) => setTokenName(e.target.value)} placeholder="Enter the name of token you want to mint" className="flex h-10 w-full rounded-md  px-3 py-2 text-sm  bg-black text-white mt-2  " />
-
-                  <label className=" flex pt-2 ">Symbol</label>
-
-                  <Input type="text" placeholder="Enter desired Symbol" onChange={(e) => setSymbol(e.target.value)} className="flex h-10 w-full rounded-md  px-3 py-2 text-sm  bg-black text-white mt-2  " />
+                  <input type="text" placeholder="Enter desired Symbol" onChange={(e) => setSymbol(e.target.value)} className="flex h-10 w-full rounded-md  px-3 py-2 text-sm  bg-black text-white mt-2 border " />
                   <label className=" flex pt-2">Number of tokens to mint</label>
 
 
 
-                  <Input type="number" min="0" value={quantity} onChange={(e) => setQuantity(parseInt(e.target.value))} placeholder="Enter the number of tokens you want to mint" className="flex h-10 w-full rounded-md  px-3 py-2 text-sm  bg-black text-white mt-2  " />
+                  <input type="number" min="0" value={quantity} onChange={(e) => setQuantity(parseInt(e.target.value))} placeholder="Enter the number of tokens you want to mint" className="flex h-10 w-full rounded-md border  px-3 py-2 text-sm  bg-black text-white mt-2  " />
 
                   <label className=" flex pt-2">Number of decimals</label>
 
-                  <Input type="number" min="0" value={decimals} onChange={(e) => setDecimals(parseInt(e.target.value))} placeholder="Enter the number of decimals" className="flex h-10 w-full rounded-md  px-3 py-2 text-sm  bg-black text-white mt-2  " />
+                  <input type="number" min="0" value={decimals} onChange={(e) => setDecimals(parseInt(e.target.value))} placeholder="Enter the number of decimals" className="flex h-10 w-full rounded-md border  px-3 py-2 text-sm  bg-black text-white mt-2  " />
 
 
-                  <div className="mt-5 mb-2   flex  text-2xl">Meta data</div>
-                  <Tabs defaultValue="password" className=" ">
-                    <TabsList>
-  <TabsTrigger value="password">Create Metadata</TabsTrigger>
-                      <TabsTrigger value="account">Use Existing Metadata URL</TabsTrigger>
-                    
-                    </TabsList>
- <TabsContent value="password" onClick={() => { setMetadataMethod('upload'), setMetadataURL(''), setFile(undefined), setFileName('') }}>
-                      <label className="  flex">Description</label>
+                  <div className="mt-5 mb-2  flex text-2xl">Meta data</div>
+                  <div className="flex justify-center">
+                    {metadataMethod == 'upload' ?
+                      <button className="text-black mx-2 border border-rounded rounded-sm p-1 bg-white  ">Create the metadata</button>
+                      : <button className="text-white mx-2  " onClick={() => { setMetadataMethod('upload'), setMetadataURL(''), setFile(undefined), setFileName('') }}>Create the metadata</button>}
 
+                    {metadataMethod == 'url' ?
+                      <button className="text-black mx-2   border border-rounded rounded-sm p-1 bg-white  ">Use an existing medatata URL</button>
+                      : <button className="text-white mx-2 -semibold " onClick={() => { setMetadataMethod('url'), setTokenDescription('') }}>Use an existing medatata URL</button>
+                    }
+                  </div>
 
-                      <Input type="text" onChange={(e) => setTokenDescription(e.target.value)} placeholder="Enter the description of the token" className="flex h-10 w-full rounded-md  px-3 py-2 text-sm  bg-black text-white mt-2  " />
-
+                  {metadataMethod == 'url' &&
+                    <div>
                       <div>
-
-                        <label className=" mt-2 flex ">Image</label>
-                        <div className="FLEX">
-                          <label htmlFor="file" className=" text-slate-50  bg-slate-900/90 p-2 rounded dark:bg-slate-50 hover:bg-black  dark:text-slate-900 "
-                          >
-                            Upload image
-                          </label>
-                        </div>
-                        <input
-                          id="file"
-                          type="file"
-                          name="file"
-                          accept="image/*"
-                          onChange={handleFileChange}
-                          style={{ display: 'none' }} />
+                        <label className="underline mt-2 flex font-bold">Metadata Url</label>
+                        <input className="flex h-10 w-full  border px-3 py-2 text-sm  bg-black text-white mt-2"
+                          type="text"
+                          placeholder="Metadata Url"
+                          onChange={(e) => setMetadataURL(e.target.value)}
+                        />
                       </div>
-                     <div className="flex items-center border rounded-md p-2 mt-4">
-  <div className="flex items-center justify-between w-full">
-    <div className="flex items-center">
-      {(img === true)? (
-  <Image
-    src={`data:image/jpeg;base64,${base64Image}`}
-    className="rounded-full h-10 w-10 border"
-    alt=""
-    width={50}
-    height={50}
-  />
-) : (
-  <img
-    src="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBwgICAgICAgIBwcHBwoHBwcHBw8ICQgKFREWFhURExMYKCggGCYlGxMTITEhJSkrLi4uFx8zODM4NygtLisBCgoKBQUFDgUFDisZExkrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrK//AABEIAKgBLAMBIgACEQEDEQH/xAAXAAEBAQEAAAAAAAAAAAAAAAAAAQIH/8QAHhABAQACAQUBAAAAAAAAAAAAAAERITECQVFhoYH/xAAUAQEAAAAAAAAAAAAAAAAAAAAA/8QAFBEBAAAAAAAAAAAAAAAAAAAAAP/aAAwDAQACEQMRAD8A4aKAioAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAvTjO8yepmlQAABQAEVABQEFQAa6cd0BBQEAAAAFQAAAAAFBAAAAAAAAAAAAAAUIAIoAAAAAUKCAAAAAAqAAAAtiKCAAAoICggAAAAAAAAAKAAAAAAAAioCoAKCAKIAAACgCAAAAAAAAAAAAAAAAAKsmb4QAAAAAABBQEFQAVAAAFQAAAAAAAAAAAAAAAAAAAABQAAAAAAAEVABQAvIAgoCAAAACoAAC638QAAAAAAAAAFJcXMLsEABQAAAAAEFBAAUAEAAAAAAAAFuNYzxvPlAAAAUEAAAAAAABUAAAFRagAAKIAAAC5+ICoAAAAAAAAACoAAAAAAAAAAAAAAAKgAAAAAAALbbz4wgAACoAAAAoICggAAqALhABdo109VksnfkGQAAAAAAAFQAAAAAAAAAFQAAAAAAAAAAAAAAAAAWIAAAAAC0EXCLLve/V7ggt5/UAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABUAAAH//Z" // Replace with your desired placeholder image link
-    className="rounded-full h-10 w-10 border"
-    alt=""
-    width={50}
-    height={50}
-  />
-)}
-      <div className="ml-2">
-        <p className="mb-1">{tokenName}</p>
-        <p className="">{symbol}</p>
-      </div>
-    </div>
-    <div className="text-right ">
-      <p className="pr-2">$69</p>
-      <p className="text-green-400">250%</p>
-    </div>
-  </div>
-</div>
-                    </TabsContent>
-                    <TabsContent value="account" onClick={() => { setMetadataMethod('url'), setTokenDescription('') }}>
-                      <label className=" mt-2 flex">Metadata URL</label>
 
-                      <Input type="text" onChange={(e) => setMetadataURL(e.target.value)} placeholder="Enter the metadata URL" className="flex h-10 w-full rounded-md  px-3 py-2 text-sm  bg-black text-white mt-2  " />
+                    </div>
+                  }
 
-                    </TabsContent>
-                   
-                  </Tabs>
+                  {metadataMethod == 'upload' &&
+                    <div>
+                      <div>
+                        <label className="underline mt-2 flex font-bold">Description</label>
+                        <input className="flex h-10 w-full  border px-3 py-2 text-sm  bg-black text-white mt-2 "
+                          type="text"
+                          placeholder="Description of the token/project"
+                          onChange={(e) => setTokenDescription(e.target.value)}
+                        />
+                      </div>
+                      <div>
+                        <label className="underline mt-2 flex font-bold">Image</label>
+                        <label htmlFor="file" className=" text-slate-50  bg-slate-900/90 p-2 rounded dark:bg-slate-50 hover:bg-black  dark:text-slate-900">
+                          Upload image
+                          <input
+                            id="file"
+                            type="file"
+                            name="file"
+                            accept="image/*, video/*"
+                            onChange={handleFileChange}
+                            style={{ display: 'none' }} />
+                        </label>
+                        {fileName != '' && <div className="mt-2" >{fileName}</div>}
+                      </div>
+                    </div>
+                  }
                   <div className="mt-5 mb-2  flex ">Authority</div>
                   <div className="flex justify-center mb-4 ">
 
@@ -210,6 +185,37 @@ const Heh: FC = ({ }) => {
                       checked={isChecked}
                       onChange={(e) => setIsChecked(!isChecked)}
                     />
+                  </div>
+                  <div className="flex items-center border rounded-md p-2 mt-4">
+                    <div className="flex items-center justify-between w-full">
+                      <div className="flex items-center">
+                        {(img === true) ? (
+                          <Image
+                            src={`data:image/jpeg;base64,${base64Image}`}
+                            className="rounded-full h-10 w-10 border"
+                            alt=""
+                            width={50}
+                            height={50}
+                          />
+                        ) : (
+                          <img
+                            src="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBwgICAgICAgIBwcHBwoHBwcHBw8ICQgKFREWFhURExMYKCggGCYlGxMTITEhJSkrLi4uFx8zODM4NygtLisBCgoKBQUFDgUFDisZExkrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrK//AABEIAKgBLAMBIgACEQEDEQH/xAAXAAEBAQEAAAAAAAAAAAAAAAAAAQIH/8QAHhABAQACAQUBAAAAAAAAAAAAAAERITECQVFhoYH/xAAUAQEAAAAAAAAAAAAAAAAAAAAA/8QAFBEBAAAAAAAAAAAAAAAAAAAAAP/aAAwDAQACEQMRAD8A4aKAioAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAvTjO8yepmlQAABQAEVABQEFQAa6cd0BBQEAAAAFQAAAAAFBAAAAAAAAAAAAAAUIAIoAAAAAUKCAAAAAAqAAAAtiKCAAAoICggAAAAAAAAAKAAAAAAAAioCoAKCAKIAAACgCAAAAAAAAAAAAAAAAAKsmb4QAAAAAABBQEFQAVAAAFQAAAAAAAAAAAAAAAAAAAABQAAAAAAAEVABQAvIAgoCAAAACoAAC638QAAAAAAAAAFJcXMLsEABQAAAAAEFBAAUAEAAAAAAAAFuNYzxvPlAAAAUEAAAAAAABUAAAFRagAAKIAAAC5+ICoAAAAAAAAACoAAAAAAAAAAAAAAAKgAAAAAAALbbz4wgAACoAAAAoICggAAqALhABdo109VksnfkGQAAAAAAAFQAAAAAAAAAFQAAAAAAAAAAAAAAAAAWIAAAAAC0EXCLLve/V7ggt5/UAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABUAAAH//Z" // Replace with your desired placeholder image link
+                            className="rounded-full h-10 w-10 border"
+                            alt=""
+                            width={50}
+                            height={50}
+                          />
+                        )}
+                        <div className="ml-2">
+                          <p className="mb-1">{tokenName}</p>
+                          <p className="">{symbol}</p>
+                        </div>
+                      </div>
+                      <div className="text-right ">
+                        <p className="pr-2">$69</p>
+                        <p className="text-green-400">250%</p>
+                      </div>
+                    </div>
                   </div>
                 </div>
                 <CreateTokenButton connection={connection} publicKey={publicKey} wallet={wallet} quantity={quantity} decimals={decimals} isChecked={isChecked} tokenName={tokenName} symbol={symbol} metadataURL={metadataURL} description={tokenDescription} file={file} metadataMethod={metadataMethod} wltAdr={wltAdr} />
